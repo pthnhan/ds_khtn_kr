09e4c946622ee191a8dc9df1319e38724bdd4d9b
pthnhan <nhanmath97@gmail.com>
2021-07-15 05:57:50 +0700

    data S&P500 OHLCV

---
+++ data/split_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv("/home/pthnhan/Desktop/khtn/ds_khtn_kr/data/snp500_ohlcv.csv")
+sym = set(data.symbol.to_list())
+print(len(sym))
+for s in sym:
+    print(s)
+    output = data[data.symbol == s]
+    output.to_csv("/home/pthnhan/Desktop/khtn/ds_khtn_kr/data/{}.csv".format(s), index=False)
