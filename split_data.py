7e8b41973f2137e3d5a1b1d2b44a15048c90203c
pthnhan <nhanmath97@gmail.com>
2021-07-15 05:58:57 +0700

    data S&P500 OHLCV

--- split_data.py
+++ split_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv("/home/pthnhan/Desktop/khtn/ds_khtn_kr/data/snp500_ohlcv.csv")
+data = pd.read_csv("/data/snp500_ohlcv.csv")
 sym = set(data.symbol.to_list())
 print(len(sym))
 for s in sym:
